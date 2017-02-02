1987a5e0d301f809eb70c9521795ead4d71fdf7f
Wahiba Khadri <wahiba@bleech.de>
2017-02-02 17:10:55 +0100

    refactor(functions.php): removed logs

--- Components/TeaserGrid/functions.php
+++ Components/TeaserGrid/functions.php
@@ -1,11 +1,9 @@
 <?php
 namespace Flynt\Components\TeaserGrid;
 
-use Flynt\Utils\Log;
 use Timber\Post;
 
 add_filter('Flynt/addComponentData?name=TeaserGrid', function ($data) {
-  // Log::pp($data);
   if ($data['teaserItems']) {
     $data['teaserItems'] = array_map(function ($item) {
       if ($item['linkType'] == 'internalLink') {
@@ -14,6 +12,5 @@ add_filter('Flynt/addComponentData?name=TeaserGrid', function ($data) {
       return $item;
     }, $data['teaserItems']);
   }
-  // Log::pp($data);
   return $data;
 });
