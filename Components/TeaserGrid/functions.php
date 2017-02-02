f8806cc587ba825887527219b97647f060d45bd5
Wahiba Khadri <wahiba@bleech.de>
2017-02-02 17:09:50 +0100

    fix(Calling the teasers the right way): Calling the posts now via Timber

--- Components/TeaserGrid/functions.php
+++ Components/TeaserGrid/functions.php
@@ -1,20 +1,19 @@
 <?php
 namespace Flynt\Components\TeaserGrid;
 
-use Flynt\DataFilters\MainQuery;
-use Flynt\Helpers\Log;
-use Flynt\Helpers\Utils;
-use Flynt\Features\Components\Component;
+use Flynt\Utils\Log;
+use Timber\Post;
 
 add_filter('Flynt/addComponentData?name=TeaserGrid', function ($data) {
-  $data['teaserItems'] = array_map(function ($item) {
-
-    if ($item['linkType'] == 'internalLink') {
-      // $item['post'] = MainQuery::addAdditionalDefaultDataSinglePost($item['post']);
-    }
-
-    return $item;
-  }, $data['teaserItems']);
-
+  // Log::pp($data);
+  if ($data['teaserItems']) {
+    $data['teaserItems'] = array_map(function ($item) {
+      if ($item['linkType'] == 'internalLink') {
+        $item['post'] = new Post($item['post']);
+      }
+      return $item;
+    }, $data['teaserItems']);
+  }
+  // Log::pp($data);
   return $data;
 });
