8652a51abac7fe60e49ac8a5aa5acd259ea5d47e
Wahiba Khadri <wahiba@bleech.de>
2017-02-23 16:54:33 +0100

    feat(BaseComponent/MainNavigation/BrandLogo): Added a brandLogo image field
    Global option image field, calling it inside the component before the navigation

--- Components/MainNavigation/functions.php
+++ Components/MainNavigation/functions.php
@@ -2,9 +2,12 @@
 
 namespace Flynt\Components\MainNavigation;
 
+use Flynt\Features\Acf\OptionPages;
 use Timber\Menu;
 
 add_filter('Flynt/addComponentData?name=MainNavigation', function ($data) {
+  $data['brandLogo'] = OptionPages::getOption('options', 'component', 'MainNavigation', 'brandLogo');
+  $data['homeUrl'] = home_url();
   $data['menuSlug'] = !empty($data['menuSlug']) ? $data['menuSlug'] : '';
   $data['menu'] = new Menu($data['menuSlug']);
   return $data;
