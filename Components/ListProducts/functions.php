227e2f86c1728e0682b8a0ab8a0aa01ba136857c
Wahiba Khadri <wahiba@bleech.de>
2017-05-02 16:39:32 +0200

    refactor(Components/ListProducts): added globalOptions and translatableOptions
    added globalOptions and translatableOptions to component

--- Components/ListProducts/functions.php
+++ Components/ListProducts/functions.php
@@ -12,6 +12,12 @@ add_filter('Flynt/addComponentData?name=ListProducts', function ($data) {
     $defaultCount = OptionPages::getOption('globalOptions', 'customPostType', 'product', 'defaultProductsNumber');
     $data['productTypesLabel'] = OptionPages::getOption('translatableOptions', 'customPostType', 'product', 'productTypesLabel');
     $data['overviewPage'] = OptionPages::getOption('translatableOptions', 'customPostType', 'product', 'overviewPage');
+    if (isset($data['defaultImage']) && !empty($data['defaultImage'])) {
+      $data['image'] = $data['defaultImage'];
+    }
+    if (isset($data['defaultTitleText']) && !empty($data['defaultTitleText'])) {
+      $data['titleText'] = $data['defaultTitleText'];
+    }
     $args = [
         'post_type' => 'product',
         'posts_per_page' => ($defaultCount) ? $defaultCount : 10,
