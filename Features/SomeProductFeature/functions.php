0d40467b181844b51f156450b4543120b18e116d
Wahiba Khadri <wahiba@bleech.de>
2017-05-04 14:24:09 +0200

    feat(Features/SomeProductFeature/functions): Added an admin notice
    Added an admin notice if the someFeatureNumber field is not filled

--- Features/SomeProductFeature/functions.php
+++ Features/SomeProductFeature/functions.php
@@ -1,1 +1,19 @@
 <?php
+
+use Flynt\Features\Acf\OptionPages;
+use Flynt\Features\AdminNotices\AdminNoticeManager;
+
+add_action('init', function () {
+    $someFeatureNumber = OptionPages::getOption('globalOptions', 'feature', 'someProductFeature', 'someFeatureNumber');
+    if (!$someFeatureNumber) {
+        $manager = AdminNoticeManager::getInstance();
+        $messages = ['Please fill Some Feature Number'];
+        $options = [
+          'type' => 'error',
+          'title' => 'Some Product Feature',
+          'dismissible' => true,
+          'filenames' => 'functions.php'
+        ];
+        $manager->addNotice($messages, $options);
+    }
+});
