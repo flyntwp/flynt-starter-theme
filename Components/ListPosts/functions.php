cbc96b487cdfa0225300a9f505007e10ce96a244
Wahiba Khadri <wahiba@bleech.de>
2017-05-31 17:41:18 +0200

    refactor(Components/ListPosts): Calling manually the list of posts
    In order to use the component inside flexible content

--- Components/ListPosts/functions.php
+++ Components/ListPosts/functions.php
@@ -3,6 +3,7 @@
 namespace Flynt\Components\ListPosts;
 
 use Flynt\Features\Components\Component;
+use Timber\Timber;
 
 add_action('wp_enqueue_scripts', function () {
     Component::enqueueAssets('ListPosts');
@@ -10,11 +11,11 @@ add_action('wp_enqueue_scripts', function () {
 
 add_filter('Flynt/addComponentData?name=ListPosts', function ($data, $parentData) {
     $data['isArchive'] = is_home() || is_archive();
-
     $data['pagination'] = (isset($parentData['pagination'])) ? $parentData['pagination'] : null;
-    if (!isset($data['posts']) && isset($parentData['posts'])) {
-        $data['posts'] = $parentData['posts'];
-    }
-
+    $args = [
+        'post_status' => 'publish',
+        'post_type' => 'post'
+    ];
+    $data['posts'] = Timber::get_posts($args);
     return $data;
 }, 10, 2);
