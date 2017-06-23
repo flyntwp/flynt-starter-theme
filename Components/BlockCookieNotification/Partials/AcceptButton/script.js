3d6e964126a7ad67cd1f9eaa79a8c7a59957d401
Dominik Biedebach <dominik.biedebach@gmail.com>
2017-06-23 18:25:21 +0200

    refactor(AcceptButton Partial): Refactored and removed linting

--- Components/BlockCookieNotification/Partials/AcceptButton/script.js
+++ Components/BlockCookieNotification/Partials/AcceptButton/script.js
@@ -1,38 +1,34 @@
-import $ from 'jquery'
-
 export function getOptions (optionContainer) {
-    return {
-      expireDays: 7,
-      cookieName: 'cookies_accepted'
-    }
+  return {
+    expireDays: 7,
+    cookieName: 'cookies_accepted'
   }
+}
 
 export function acceptCookies ($container, cookieName, expireDays) {
   return function (e) {
     const date = new Date()
-    date.setTime(date.getTime() + (expireDays*24*60*60*1000))
-    document.cookie = cookieName + "=true; expires=" + date.toGMTString()
-
+    date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000))
+    document.cookie = cookieName + '=true; expires=' + date.toGMTString()
     $container.remove()
   }
 }
 
 export function checkCookies ($container, cookieName) {
-    const cookiesAccepted = getCookieValue(cookieName)
-    
-    if (!cookiesAccepted) {
-      $container.addClass('cookieNotification-isVisible')
-    } else {
-      $container.remove()
-    }
+  const cookiesAccepted = getCookieValue(cookieName)
+
+  if (!cookiesAccepted) {
+    $container.addClass('cookieNotification-isVisible')
+  } else {
+    $container.remove()
   }
-
-
+}
 
 function getCookieValue (cookie) {
-  const value = "; " + document.cookie
+  const value = '; ' + document.cookie
   const parts = value.split('; ' + cookie + '=')
-  if (parts.length == 2) {
+
+  if (parts.length === 2) {
     return parts.pop().split(';').shift()
   } else {
     return false
