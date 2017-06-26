aed0cde94ceb423196f327b4f503e35694a61052
Dominik Biedebach <dominik.biedebach@gmail.com>
2017-06-26 11:44:13 +0200

    revert(GoogleAnalytics): Reverted GoogleAnalytics Feature

--- Features/GoogleAnalytics/functions.php
+++ Features/GoogleAnalytics/functions.php
@@ -2,13 +2,10 @@
 
 namespace Flynt\Features\GoogleAnalytics;
 
-define(__NAMESPACE__ . '\NS', __NAMESPACE__ . '\\');
-
 require_once __DIR__ . '/GoogleAnalytics.php';
 
 use Flynt\Features\GoogleAnalytics\GoogleAnalytics;
 use Flynt\Utils\Feature;
-use Flynt\Utils\Asset;
 use Flynt\Features\Acf\OptionPages;
 
 add_action('init', 'Flynt\Features\GoogleAnalytics\init', 100);
@@ -16,29 +13,7 @@ add_action('init', 'Flynt\Features\GoogleAnalytics\init', 100);
 function init()
 {
     $googleAnalyticsOptions = OptionPages::get('globalOptions', 'feature', 'GoogleAnalytics');
-    $googleAnalyticsOptionsTranslatable = OptionPages::get('translatableOptions', 'feature', 'GoogleAnalytics');
-
     if ($googleAnalyticsOptions) {
         new GoogleAnalytics($googleAnalyticsOptions);
     }
-
-    Asset::enqueue([
-        'type' => 'script',
-        'name' => 'js-cookie',
-        'path' => 'vendor/js-cookie.js'
-    ]);
-
-    Asset::enqueue([
-        'type' => 'script',
-        'name' => 'Flynt/Features/GoogleAnalytics',
-        'path' => 'Features/GoogleAnalytics/script.js',
-        'dependencies' => ['jquery', 'js-cookie']
-    ]);
-
-    $data = [
-        'gaId' => $googleAnalyticsOptions['gaId'],
-        'confirm' => (isset($googleAnalyticsOptionsTranslatable['optOutConfirm'])) ? $googleAnalyticsOptionsTranslatable['optOutConfirm'] : '',
-        'success' => (isset($googleAnalyticsOptionsTranslatable['optOutSuccess'])) ? $googleAnalyticsOptionsTranslatable['optOutSuccess'] : ''
-    ];
-    wp_localize_script('Flynt/Features/GoogleAnalytics', 'wpData', $data);
 }
