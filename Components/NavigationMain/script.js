71e57acde0f3b9d4694aff7e508c157dee58a43f
Dominik Biedebach <dominik.biedebach@gmail.com>
2017-06-27 12:13:56 +0200

    refactor(BaseComponent-MainNavigation): Implemented Concept for Navigation Levels in styles

--- Components/NavigationMain/script.js
+++ Components/NavigationMain/script.js
@@ -5,23 +5,49 @@ class NavigationMain extends window.HTMLElement {
     self = super(self)
     self.$ = $(self)
     self.resolveElements()
+    self.setOptions()
     return self
   }
 
   resolveElements () {
     this.$hamburger = $('.hamburger', this)
     this.$navigation = $('.navigationMain', this)
+    this.$menu = $('.menu', this)
     this.$body = $('body')
+  }
+
+  setOptions () {
+    this.options = {
+      noTransitionClass: 'menu-hasNoTransition',
+      activeMenuClass: 'navigationMain-isActive'
+    }
+
+    this.enableTransitionTimeout = false
   }
 
   connectedCallback () {
     this.$.on('click', '.hamburger', this.triggerMenu)
+    $(window).on('resize', this.handleTransitionState)
   }
 
   triggerMenu = (e) => {
     e.preventDefault()
-    this.$navigation.toggleClass('navigationMain-isActive')
-    this.$body.toggleClass('navigationMain-isActive')
+    this.$navigation.toggleClass(this.options.activeMenuClass)
+    this.$body.toggleClass(this.options.activeMenuClass)
+  }
+
+  handleTransitionState = () => {
+    this.disableMenuTransition()
+    clearTimeout(this.enableTransitionTimeout)
+    this.enableTransitionTimeout = setTimeout(this.enableMenuTransition, 150)
+  }
+
+  disableMenuTransition () {
+    this.$menu.addClass(this.options.noTransitionClass)
+  }
+
+  enableMenuTransition = () => {
+    this.$menu.removeClass(this.options.noTransitionClass)
   }
 }
 
