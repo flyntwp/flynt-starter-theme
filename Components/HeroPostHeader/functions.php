37e6c7080d1980483ec153bd61b6d6259a888cf0
Dominik Biedebach <dominik.biedebach@gmail.com>
2017-07-03 16:50:45 +0200

    refactor(HeroPostHeader): Added Screenshots, removed provider.json

--- Components/HeroPostHeader/functions.php
+++ Components/HeroPostHeader/functions.php
@@ -22,7 +22,29 @@ add_filter('Flynt/addComponentData?name=HeroPostHeader', function ($data) {
 
 function getShareProviders($postTitle, $postUrl, $postExcerpt)
 {
-    $providers = json_decode(file_get_contents(Asset::requirePath('Components/HeroPostHeader/Partials/Share/providers.json')), true);
+    $providers = [
+        [
+            'title' => 'Facebook',
+            'class' => 'share-facebook',
+            'url' => 'https://www.facebook.com/sharer/sharer.php?u=$url'
+        ],
+        [
+            'title' => 'Twitter',
+            'class' => 'share-twitter',
+            'url' => 'https://twitter.com/home?status=$url'
+        ],
+        [
+            'title' => 'Google+',
+            'class' => 'share-google',
+            'url' => 'https://plus.google.com/share?url=$url'
+        ],
+        [
+            'title' => 'Linkedin',
+            'class' => 'share-linkedin',
+            'url' => 'https://www.linkedin.com/shareArticle?mini=true&url=$url&title=$title&summary=$summary'
+        ]
+    ];
+
     $providers = array_map(function($provider) use ($postTitle, $postUrl, $postExcerpt) {
         $shareUrl = str_replace('$url', $postUrl, $provider['url']);
         $shareUrl = str_replace('$title', $postTitle, $shareUrl);
