4cfe9d5856a3c0207b6d73013a5b90fb08d9527c
Doğa Gürdal <Qakulukiam@users.noreply.github.com>
2018-11-02 10:31:20 +0100

    refactor(lib/Init): remove post formats (#290)
    These don't need to be added by default and should be added where necessary.

--- lib/Init.php
+++ lib/Init.php
@@ -75,7 +75,6 @@ function initTheme()
     // WP Stuff
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
-    add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio'));
 
     add_theme_support('flynt-password-form');
     add_theme_support('flynt-external-script-loader');
